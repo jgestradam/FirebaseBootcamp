7c6da3e763fa7ba286e944d7cd3f81307da13ddc
jgestradam <jgestradam@unal.edu.co>
2023-03-13 20:21:57 -0500

    Initial Commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Joseph Estrada on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
